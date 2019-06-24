93ee387441e8b9a95dddb7b4e95a1a7a89509606
bpdavitt <bpdavitt@gmail.com>
2019-06-24 14:18:33 -0500

    Add status bar below board to declare a winner or let players know they are both losers.

--- challenge_1/app.js
+++ challenge_1/app.js
@@ -13,6 +13,7 @@ const boardData = {
 };
 
 window.onload = function () {
+  document.getElementById('game-status').textContent = `Current Move: Player ${boardData.nextMove}`;
   boxes = document.getElementsByClassName('square');
   // Add event listener for every box
   for (let i = 0; i < boxes.length; i++) {
@@ -25,6 +26,7 @@ window.onload = function () {
   }
 }
 
+// Check to see if this spot has already been 'taken'
 const handleClick = (id) => {
   if (validMove(id)) {
     handleMove(id);
@@ -37,10 +39,26 @@ const validMove = (id) => {
   return boardData[id] === undefined;    
 }
 
+//Upon valid move this function will update the boardData object and appropriate <div>
 const handleMove = (id) => {
   const selectedSquare = document.getElementById(id);
   console.log(boardData);
   selectedSquare.textContent = boardData.nextMove;
+  boardData[id] = boardData.nextMove;
   boardData.toggle();
   boardData.totalMoves ++;
+  document.getElementById('game-status').textContent = `Current Move: Player ${boardData.nextMove}`;
+  if (boardData.totalMoves >= 5) {
+    if(checkForWin()) {
+      console.log('Congratulations, someone won');
+    } else {
+      if (boardData.totalMoves === 9) {
+        document.getElementById('game-status').textContent = `Game Over; Everyone Is A Loser`;
+      }
+    }
+  }
+}
+
+const checkForWin = () => {
+
 }
