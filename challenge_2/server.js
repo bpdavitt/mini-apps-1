4fd8c72daffc75d24beb7e525a019e5f677e322f
bpdavitt <bpdavitt@gmail.com>
2019-06-25 10:41:06 -0500

    Server now accepting POST response to /upload_json; Input is text, can convert to JSON on server-side

--- challenge_2/server.js
+++ challenge_2/server.js
@@ -1,11 +1,19 @@
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
 const port = 3000;
 
 app.use(express.static('client'));
+app.use(bodyParser());
 
 app.get('/', (req, res) => {
     res.send('CSV Generator Loading');
 });
+
+app.post('/upload_json', (req, res) => {
+    console.log('CSV upload attempted');
+    console.log(JSON.parse(req.body.CSVdata));
+    res.send('Sorry, we are under construction');
+})
 
 app.listen(port, () => console.log(`CSV server listening at local host on port ${port}!`));
