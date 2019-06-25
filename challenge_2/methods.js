64d6e6e5bd3e17e421320d3474a407e95345e402
bpdavitt <bpdavitt@gmail.com>
2019-06-25 11:39:26 -0500

    Parse and return input object and all children. Formatting lost when sending response to page

--- challenge_2/methods.js
+++ challenge_2/methods.js
@@ -9,13 +9,22 @@ module.exports.modelJSON = (input) => {
     }
     output.push(header.join(' '));
 
-    const item = []
-    for (let keys in input) {
-            if (keys !== 'children') {
-                item.push(input[keys]);
-            }
-    }
-    output.push(item.join(' '));
+    output.push(keyParser(input));
     // console.log(output);
+    return output.join('\n');
+}
+
+const keyParser = (obj) => {
+    const output = []
+    const oneLine = [];
+    for (let keys in obj) {
+        if (keys !== 'children') {
+            oneLine.push(obj[keys])
+        }
+    }
+    output.push(oneLine.join(' '));
+    for (let i = 0; i < obj.children.length; i ++) {
+        output.push(keyParser(obj.children[i]));
+    }
     return output.join('\n');
 }
