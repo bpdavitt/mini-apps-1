f2c022583c82d7f63687ad0be6e2a63a0586c727
bpdavitt <bpdavitt@gmail.com>
2019-06-25 15:45:10 -0500

    Users may now upload a file to be parsed.

--- challenge_2/server.js
+++ challenge_2/server.js
@@ -1,45 +1,83 @@
 const express = require('express');
 const app = express();
-const bodyParser = require('body-parser');
 const port = 3000;
 const method = require('./methods');
+const fs = require('fs');
+const multer = require('multer');
+const upload = multer({ dest: 'uploads/' });
+
 
 app.use(express.static('client'));
 
-app.use(bodyParser());
 
 app.get('/', (req, res) => {
     res.send('CSV Generator Loading');
 });
 
-app.post('/', (req, res) => {
+// FUTURE WORK: Multer should be able to deal w/ text or null file as well
+
+
+app.post('/', upload.single('CSVdata'), (req, res, next) => {
     console.log('CSV upload attempted');
+    console.log(req.file);
+    fs.readFile(req.file.path, 'utf-8', (err, data) => {
+        if (err) {
+            res.send('Error while reading file');
+        } else {
+            const output = method.modelJSON(data);
+            console.log(output);
+            res.location('localhost:3000/').send(`
+                <!DOCTYPE html>
+
+                <html>
+                <head>
+                    <title>CSV Report Generator</title>
+                    <link rel="stylesheet" type="text/css" href="style.css">
+                    <style>.CSVoutput{white-space: pre-wrap} </style>
+                </head>
+
+                <body>
+                    <h1>Enter CSV Report</h1>
+                    <form action="/" method="post" enctype="multipart/form-data">
+                        <label for="POST-CSV">Paste CSV Data Here:</label>
+                        <input type="file" name="CSVdata" id="POST-CSV" required>                        
+                        <input type="submit" value="Submit">
+                    </form>
+                    <div class="CSVoutput">${output}</div>
+
+                    <script type ="text/javascript" src="./app.js"></script>
+                </body>
+                </html>`
+            )
+        }
+    })
+    // res.send('Halp');
     // console.log(JSON.parse(req.body.CSVdata));
-    const output = method.modelJSON(req.body.CSVdata);
-    console.log(output);
-    res.location('localhost:3000/').send(`
-    <!DOCTYPE html>
+    // const output = method.modelJSON(req.body.CSVdata);
+    // console.log(output);
+    // res.location('localhost:3000/').send(`
+    // <!DOCTYPE html>
 
-    <html>
-    <head>
-        <title>CSV Report Generator</title>
-        <link rel="stylesheet" type="text/css" href="style.css">
-        <style>.CSVoutput{white-space: pre-wrap} </style>
-    </head>
+    // <html>
+    // <head>
+    //     <title>CSV Report Generator</title>
+    //     <link rel="stylesheet" type="text/css" href="style.css">
+    //     <style>.CSVoutput{white-space: pre-wrap} </style>
+    // </head>
 
-    <body>
-        <h1>Enter CSV Report</h1>
-        <form action="/" method="post">
-            <label for="POST-CSV">Paste CSV Data Here:</label>
-            <input type="text" name="CSVdata" id="POST-CSV" required>
-            <input type="submit" value="Submit">
-        </form>
-        <div class="CSVoutput">${output}</div>
+    // <body>
+    //     <h1>Enter CSV Report</h1>
+    //     <form action="/" method="post">
+    //         <label for="POST-CSV">Paste CSV Data Here:</label>
+    //         <input type="text" name="CSVdata" id="POST-CSV" required>
+    //         <input type="submit" value="Submit">
+    //     </form>
+    //     <div class="CSVoutput">${output}</div>
 
-        <script type ="text/javascript" src="./app.js"></script>
-    </body>
-    </html>
-    `)
+    //     <script type ="text/javascript" src="./app.js"></script>
+    // </body>
+    // </html>
+    // `)
 })
 
 app.listen(port, () => console.log(`CSV server listening at local host on port ${port}!`));
