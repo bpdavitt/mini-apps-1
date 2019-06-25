ab821e7c2fce3c4a1cd16e42de8019021f75e72d
bpdavitt <bpdavitt@gmail.com>
2019-06-25 12:35:21 -0500

    On response serve up entire HTML page along with data from previous report. Will refactor out once single page app segment reached.

--- challenge_2/server.js
+++ challenge_2/server.js
@@ -5,19 +5,41 @@ const port = 3000;
 const method = require('./methods');
 
 app.use(express.static('client'));
+
 app.use(bodyParser());
 
 app.get('/', (req, res) => {
     res.send('CSV Generator Loading');
 });
 
-app.post('/upload_json', (req, res) => {
+app.post('/', (req, res) => {
     console.log('CSV upload attempted');
     // console.log(JSON.parse(req.body.CSVdata));
     const output = method.modelJSON(req.body.CSVdata);
     console.log(output);
-    res.send(output);
-    // res.send('Data received')
+    res.location('localhost:3000/').send(`
+    <!DOCTYPE html>
+
+    <html>
+    <head>
+        <title>CSV Report Generator</title>
+        <link rel="stylesheet" type="text/css" href="style.css">
+        <style>.CSVoutput{white-space: pre-wrap} </style>
+    </head>
+
+    <body>
+        <h1>Enter CSV Report</h1>
+        <form action="/" method="post">
+            <label for="POST-CSV">Paste CSV Data Here:</label>
+            <input type="text" name="CSVdata" id="POST-CSV" required>
+            <input type="submit" value="Submit">
+        </form>
+        <div class="CSVoutput">${output}</div>
+
+        <script type ="text/javascript" src="./app.js"></script>
+    </body>
+    </html>
+    `)
 })
 
 app.listen(port, () => console.log(`CSV server listening at local host on port ${port}!`));
