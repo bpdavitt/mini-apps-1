4482d521b87b7147b0445fe4eb6d4a99ef5b7e73
bpdavitt <bpdavitt@gmail.com>
2019-06-25 10:25:49 -0500

    Initial set-up for challenge 2; Basic HTML created, server running and serving HTML

--- challenge_2/server.js
+++ challenge_2/server.js
@@ -1,2 +1,11 @@
+const express = require('express');
+const app = express();
+const port = 3000;
 
+app.use(express.static('client'));
 
+app.get('/', (req, res) => {
+    res.send('CSV Generator Loading');
+});
+
+app.listen(port, () => console.log(`CSV server listening at local host on port ${port}!`));
