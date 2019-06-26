b29d1c5b3e59886e7a0ccf225c6e855226cc2c20
bpdavitt <bpdavitt@gmail.com>
2019-06-26 09:28:39 -0500

    Create basic HTML page, initialize server to serve up that HTML;

--- challenge_3/server.js
+++ challenge_3/server.js
@@ -1,2 +1,12 @@
+const express = require('express');
+const app = express();
+const port = 3000;
 
+app.use(express.static('public'));
+app.use(express.static('client'));
 
+app.get('/', (req, res) => {
+  res.send('Hello')
+});
+
+app.listen(port, () => console.log('Shopping app server listening on port ' + port));
