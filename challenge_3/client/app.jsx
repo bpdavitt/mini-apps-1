81ec4ee5d5006a357931e75cc6ffbf34de3439b6
bpdavitt <bpdavitt@gmail.com>
2019-06-26 12:50:58 -0500

    Implement React App components; May cycle through all pages; no data transmitted to server at this point

--- challenge_3/client/app.jsx
+++ challenge_3/client/app.jsx
@@ -1,6 +1,105 @@
 console.log('Is Babel working?')
 
+const App = () => (
+    <div>
+        <ShoppingApp />
+    </div>
+);
+
+class ShoppingApp extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            currentPage: undefined
+        }
+        this.handleClick = this.handleClick.bind(this)
+    }
+
+    handleClick(nextPage, formData) {
+        if (formData !== null) {
+            formData.preventDefault();
+            formData = formData.target;
+            const data = {}
+            for (let i = 0; i < formData.length - 1; i++) {
+                data[formData[i].name]= formData[i].value;
+            }
+            console.log(data);
+        }
+        this.setState({ currentPage: nextPage });
+    }
+
+    render() {
+        if (this.state.currentPage === undefined) {
+            return (
+                <button onClick={() => { this.handleClick(F1, null) }}>Click Here to Give Up All Your Info</button>
+            )
+        } else {
+            return (
+                <div>
+                    <this.state.currentPage
+                        handleClick={this.handleClick}
+                    />
+                </div>
+            )
+        }
+    }
+}
+
+const F1 = props => {
+
+    return (
+        <h1>Please Enter Your Name, Email, and Password
+            <form onSubmit={(event) => {props.handleClick(F2, event)}}>
+                <input type="text" name="name" placeholder="Name" />
+                <input type="text" name="email" placeholder="Email " />
+                <input type="text" name="password" placeholder="Password" />
+                <button type="submit">Click Here To Continue</button>
+            </form>
+        </h1>
+    )
+
+}
+
+const F2 = props => {
+    return (
+        <h1>Please Enter Your Address and Phone
+            <form onSubmit={(event) => {props.handleClick(F3, event)}}>
+                <input type="text" name="address1" placeholder="Address Line 1" />
+                <input type="text" name="address2" placeholder="Address Line 2" />
+                <input type="text" name="city" placeholder="City" />
+                <input type="text" name="state" placeholder="State" />
+                <input type="text" name="zip" placeholder="Zip Code" />
+                <input type="text" name="phone" placeholder="Phone Number" />
+                <button type="submit">Click Here To Continue</button>
+            </form>
+        </h1>
+    )
+}
+
+const F3 = props => {
+    return (
+        <h1>Please Enter Your Credit Card Info
+            <form onSubmit={(event) => {props.handleClick(Confirmation, event)}}>
+                <input type="text" name="cc#" placeholder="Credit Card Number" />
+                <input type="text" name="ccExp" placeholder="Credit Card Expiration" />
+                <input type="text" name="CVV" placeholder="CVV Number" />
+                <input type="text" name="billZip" placeholder="Billing Zip Code" />
+                <button type="submit">Click Here To Continue</button>
+            </form>
+        </h1>
+    )
+}
+
+const Confirmation = props => {
+    return (
+        <h1>Please Review Your Info Prior to Submitting Order
+            <p>Some Info Here</p>
+            <button onClick={() => {props.handleClick(undefined, null)}}>Click Here to Submit Order</button>
+        </h1>
+    )
+}
+
 ReactDOM.render(
-    <h1>Hello, world!</h1>,
+    <ShoppingApp />,
     document.getElementById('app')
 );
