04093235549dc9a97e4b647b7c82d7ad496b8042
bpdavitt <bpdavitt@gmail.com>
2019-06-27 16:11:59 -0500

    Users may alternate placing pieces, pieces drop to lowest available column. No win detection, issues with styling when a row is not completely filled

--- challenge_4/client/src/index.jsx
+++ challenge_4/client/src/index.jsx
@@ -22,6 +22,30 @@ class App extends React.Component {
 
   handleClick(event) {
     console.log(event.target)
+    this.checkValidity(event.target.id);
+  }
+
+  checkValidity(location) {
+    const row = Number(location[0]);
+    const col = Number(location[1]);
+    console.log(`row: ${row} \n col: ${col}`)
+    for (let i = 6; i >= row; i--) {
+      if(this.state.board[i][col] === 0) {
+        this.placePiece(i, col);
+        break;
+      }
+    }
+  }
+
+  placePiece(row, col) {
+    const squareId = '' + row + col;
+    console.log(squareId);
+    document.getElementById(squareId).textContent = this.state.currentPlayer;
+    const currBoard = this.state.board.slice(0);
+    currBoard[row][col] = this.state.currentPlayer === 'X' ? 1 : 2;
+    const nextPlayer = this.state.currentPlayer === 'X' ? 'O' : 'X';
+    console.log(currBoard);
+    this.setState({board: currBoard, currentPlayer: nextPlayer});
   }
 
   render() {
