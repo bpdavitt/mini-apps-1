d4c5079d6c2e2dad6cdd859bd980fddf8e534b54
bpdavitt <bpdavitt@gmail.com>
2019-06-27 07:33:02 -0700

    Initialize server

--- challenge_4/server.js
+++ challenge_4/server.js
@@ -1,2 +1,9 @@
+const express = require('express');
+const app = express();
+const port = 3000;
 
+app.get('/', (req, res) => {
+  res.send('Server running')
+});
 
+app.listen(port, () => console.log(`Connect4 server listening on port ${port}!`));
